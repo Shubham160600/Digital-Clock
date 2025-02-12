da7c567e59b82ffcd937284869469b1c1ca294d2
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2025-02-12 07:17:11 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DigitalClock;
+
+//package DigitalClock;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		Clock w = new Clock();
+
+	}
+}
